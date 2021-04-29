2189a99f74ac5a38c2aaef0bcaf55e1b405ba7c1
JJin <jerkjoe61@gmail.com>
2021-04-29 02:08:12 -0700

    test

---
+++ index.ts
@@ -0,0 +1,49 @@
+import { getOctokit } from '@actions/github';
+import * as core from '@actions/core';
+import fs from 'fs';
+import { current } from './dateCount.json';
+import { Solution, solutions as issues } from './solutions/solutions';
+
+type OwnerAndRepo = {
+  owner: string;
+  repo: string;
+};
+
+const octokit = getOctokit(core.getInput('token'));
+
+const getRepoAndOwner = (): OwnerAndRepo => {
+  const [owner, repo] = core.getInput('repository').split('/');
+  return {
+    owner,
+    repo,
+  };
+};
+
+const createIssue = async ({
+  description: body,
+  tags: labels,
+  title,
+}: Solution): Promise<string> => {
+  // Create an issue
+  const { owner, repo } = getRepoAndOwner();
+  const { data: issue } = await octokit.rest.issues.create({
+    owner,
+    repo,
+    title,
+    body,
+    labels,
+  });
+
+  console.log('issue created!', issue);
+  core.info(`Created issue #${issue.number}`);
+  return issue.number;
+};
+
+createIssue(issues[current]);
+
+fs.writeFileSync(
+  'dateCount.json',
+  JSON.stringify({
+    current: current + 1,
+  })
+);
